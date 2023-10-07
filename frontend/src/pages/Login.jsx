f987b4ccda037c3299b227735727033a750ab823
Darren Bansil <106610067+libioco@users.noreply.github.com>
2023-10-07 09:48:13 -0400

    Add login, register, and addrecord api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Login = () => {
 	return (
-		<div class="auth">
+		<div class="login">
 			<h1>Login</h1>
 		</div>
 	);
